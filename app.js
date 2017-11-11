61d6afb9e5f137862538762c7144f77d2eda8aec
Ian Duke <ian@thefarmventures.com>
2017-11-11 15:17:33 -0500

    add console log

--- app.js
+++ app.js
@@ -180,6 +180,7 @@ app.post('/webhook', function (req, res) {
 }
 
 function receivedMessage(event) {
+  console.log(event);
   var senderID = event.sender.id;
   var pageID = event.recipient.id;
   var timeOfMessage = event.timestamp;
