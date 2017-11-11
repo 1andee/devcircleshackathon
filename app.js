91b98aca320b2f6045aeb10d6230809d82ea3c12
Ian Duke <ian@thefarmventures.com>
2017-11-11 11:36:10 -0500

    update token

--- app.js
+++ app.js
@@ -10,12 +10,12 @@
 /* jshint node: true, devel: true */
 'use strict';
 
-const 
+const
   bodyParser = require('body-parser'),
   config = require('config'),
   crypto = require('crypto'),
   express = require('express'),
-  https = require('https'),  
+  https = require('https'),
   request = require('request'),
   Shopify = require('shopify-api-node');
 
@@ -26,13 +26,13 @@ app.use(bodyParser.json({ verify: verifyRequestSignature }));
 app.use(express.static('public'));
 
 /*
- * Open config/default.json and set your config values before running this code. 
+ * Open config/default.json and set your config values before running this code.
  * You can also set them using environment variables.
  *
  */
 
 // App Secret can be retrieved from the App Dashboard
-const FB_APP_SECRET = (process.env.FB_APP_SECRET) ? 
+const FB_APP_SECRET = (process.env.FB_APP_SECRET) ?
   process.env.FB_APP_SECRET :
   config.get('fb_appSecret');
 
@@ -46,21 +46,21 @@ const FB_PAGE_ACCESS_TOKEN = (process.env.FB_PAGE_ACCESS_TOKEN) ?
   (process.env.FB_PAGE_ACCESS_TOKEN) :
   config.get('fb_pageAccessToken');
 
-const SHOPIFY_SHOP_NAME = (process.env.SHOP_NAME) ? 
+const SHOPIFY_SHOP_NAME = (process.env.SHOP_NAME) ?
   process.env.SHOP_NAME :
-  config.get('sh_shopName');  
+  config.get('sh_shopName');
 
-const SHOPIFY_API_KEY = (process.env.SHOP_API_KEY) ? 
+const SHOPIFY_API_KEY = (process.env.SHOP_API_KEY) ?
   process.env.SHOP_API_KEY :
-  config.get('sh_apiKey');  
+  config.get('sh_apiKey');
 
-const SHOPIFY_API_PASSWORD = (process.env.SHOP_API_PASSWORD) ? 
+const SHOPIFY_API_PASSWORD = (process.env.SHOP_API_PASSWORD) ?
   process.env.SHOP_API_PASSWORD :
-  config.get('sh_apiPassword');  
+  config.get('sh_apiPassword');
 
-const HOST_URL = (process.env.HOST_URL) ? 
+const HOST_URL = (process.env.HOST_URL) ?
   process.env.HOST_URL :
-  config.get('host_url');  
+  config.get('host_url');
 
 // make sure that everything has been properly configured
 if (!(FB_APP_SECRET && FB_VALIDATION_TOKEN && FB_PAGE_ACCESS_TOKEN && SHOPIFY_SHOP_NAME && SHOPIFY_API_KEY && SHOPIFY_API_PASSWORD && HOST_URL)) {
@@ -76,8 +76,8 @@ const shopify = new Shopify({
 
 
 /*
- * Verify that the callback came from Facebook. Using the App Secret from 
- * your App Dashboard, we can verify the signature that is sent with each 
+ * Verify that the callback came from Facebook. Using the App Secret from
+ * your App Dashboard, we can verify the signature that is sent with each
  * callback in the x-hub-signature field, located in the header.
  *
  * https://developers.facebook.com/docs/graph-api/webhooks#setup
@@ -108,7 +108,7 @@ function verifyRequestSignature(req, res, buf) {
 }
 
 /*
- * Use your own validation token. Check that the token used in the Webhook 
+ * Use your own validation token. Check that the token used in the Webhook
  * setup is the same token used here.
  *
  */
@@ -119,13 +119,13 @@ app.get('/webhook', function(req, res) {
     res.status(200).send(req.query['hub.challenge']);
   } else {
     console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);          
-  }  
+    res.sendStatus(403);
+  }
 });
 
 /**
  * serves a static page for the webview
- */ 
+ */
 app.get('/product_description', function(req, res) {
   var product_id = req.query['id'];
   if (product_id !== 'null') {
@@ -138,26 +138,26 @@ app.get('/product_description', function(req, res) {
       console.error("Error retrieving product");
       res.sendStatus(400).send("Error retrieving product");
     });
-    
+
   } else {
     console.error("Product id is required");
-    res.sendStatus(400).send("Product id is required");          
-  }  
+    res.sendStatus(400).send("Product id is required");
+  }
 });
 
 /*
  * All callbacks for Messenger are POST-ed. They will be sent to the same
  * webhook. Be sure to subscribe your app to your page to receive callbacks
- * for your page. 
+ * for your page.
  * https://developers.facebook.com/docs/messenger-platform/product-overview/setup#subscribe_app
  *
  */
 app.post('/webhook', function (req, res) {
   // You must send back a status 200 to let the Messenger Platform know that you've
-  // received the callback. Do that right away because the countdown doesn't stop when 
-  // you're paused on a breakpoint! Otherwise, the request might time out. 
+  // received the callback. Do that right away because the countdown doesn't stop when
+  // you're paused on a breakpoint! Otherwise, the request might time out.
   res.sendStatus(200);
-        
+
   var data = req.body;
 
   // Make sure this is a page subscription
@@ -173,7 +173,7 @@ app.post('/webhook', function (req, res) {
         let propertyNames = [];
         for (var prop in messagingEvent) { propertyNames.push(prop)}
         console.log("[app.post] Webhook received a messagingEvent with properties: ", propertyNames.join());
-        
+
         if (messagingEvent.message) {
           // someone sent a message
           receivedMessage(messagingEvent);
@@ -198,10 +198,10 @@ app.post('/webhook', function (req, res) {
 /*
  * Message Event
  *
- * This event is called when a message is sent to your page. The 'message' 
+ * This event is called when a message is sent to your page. The 'message'
  * object format can vary depending on the kind of message that was received.
  * Read more at https://developers.facebook.com/docs/messenger-platform/webhook-reference/message-received
- * 
+ *
  */
 function receivedMessage(event) {
   var senderID = event.sender.id;
@@ -209,11 +209,11 @@ function receivedMessage(event) {
   var timeOfMessage = event.timestamp;
   var message = event.message;
 
-  console.log("[receivedMessage] user (%d) page (%d) timestamp (%d) and message (%s)", 
+  console.log("[receivedMessage] user (%d) page (%d) timestamp (%d) and message (%s)",
     senderID, pageID, timeOfMessage, JSON.stringify(message));
 
   if (message.quick_reply) {
-    console.log("[receivedMessage] quick_reply.payload (%s)", 
+    console.log("[receivedMessage] quick_reply.payload (%s)",
       message.quick_reply.payload);
     handleQuickReplyResponse(event);
     return;
@@ -228,7 +228,7 @@ function receivedMessage(event) {
       case 'help':
         sendHelpOptionsAsButtonTemplates(senderID);
         break;
-      
+
       default:
         // otherwise, just echo it back to the sender
         sendTextMessage(senderID, messageText);
@@ -241,7 +241,7 @@ function receivedMessage(event) {
  *
  */
 function sendHelpOptionsAsButtonTemplates(recipientId) {
-  console.log("[sendHelpOptionsAsButtonTemplates] Sending the help options menu"); 
+  console.log("[sendHelpOptionsAsButtonTemplates] Sending the help options menu");
   var messageData = {
     recipient: {
       id: recipientId
@@ -258,7 +258,7 @@ function sendHelpOptionsAsButtonTemplates(recipientId) {
               "title":"Get 3 products",
               "payload":JSON.stringify({action: 'QR_GET_PRODUCT_LIST', limit: 3})
             }
-            // limit of three buttons 
+            // limit of three buttons
           ]
         }
       }
@@ -269,7 +269,7 @@ function sendHelpOptionsAsButtonTemplates(recipientId) {
 }
 
 /*
- * Someone tapped one of the Quick Reply buttons so 
+ * Someone tapped one of the Quick Reply buttons so
  * respond with the appropriate content
  *
  */
@@ -278,18 +278,18 @@ function handleQuickReplyResponse(event) {
   var pageID = event.recipient.id;
   var message = event.message;
   var quickReplyPayload = message.quick_reply.payload;
-  
-  console.log("[handleQuickReplyResponse] Handling quick reply response (%s) from sender (%d) to page (%d) with message (%s)", 
+
+  console.log("[handleQuickReplyResponse] Handling quick reply response (%s) from sender (%d) to page (%d) with message (%s)",
     quickReplyPayload, senderID, pageID, JSON.stringify(message));
-  
+
   // use branched conversation with one interaction per feature (each of which contains a variable number of content pieces)
   respondToHelpRequestWithTemplates(senderID, quickReplyPayload);
-  
+
 }
 
 /*
  * This response uses templateElements to present the user with a carousel
- * You send ALL of the content for the selected feature and they can 
+ * You send ALL of the content for the selected feature and they can
  * swipe from side to side to see it
  *
  */
@@ -343,7 +343,7 @@ function respondToHelpRequestWithTemplates(recipientId, requestForHelpOnFeature)
           });
         });
 
-        
+
         var messageData = {
           recipient: {
             id: recipientId
@@ -396,7 +396,7 @@ function respondToHelpRequestWithTemplates(recipientId, requestForHelpOnFeature)
 /*
  * Delivery Confirmation Event
  *
- * This event is sent to confirm the delivery of a message. Read more about 
+ * This event is sent to confirm the delivery of a message. Read more about
  * these fields at https://developers.facebook.com/docs/messenger-platform/webhook-reference/message-delivered
  *
  */
@@ -410,7 +410,7 @@ function receivedDeliveryConfirmation(event) {
 
   if (messageIDs) {
     messageIDs.forEach(function(messageID) {
-      console.log("[receivedDeliveryConfirmation] Message with ID %s was delivered", 
+      console.log("[receivedDeliveryConfirmation] Message with ID %s was delivered",
         messageID);
     });
   }
@@ -421,20 +421,20 @@ function receivedDeliveryConfirmation(event) {
 /*
  * Postback Event
  *
- * This event is called when a postback is tapped on a Structured Message. 
+ * This event is called when a postback is tapped on a Structured Message.
  * https://developers.facebook.com/docs/messenger-platform/webhook-reference/postback-received
- * 
+ *
  */
 function receivedPostback(event) {
   var senderID = event.sender.id;
   var recipientID = event.recipient.id;
   var timeOfPostback = event.timestamp;
 
-  // The 'payload' param is a developer-defined field which is set in a postback 
-  // button for Structured Messages. 
+  // The 'payload' param is a developer-defined field which is set in a postback
+  // button for Structured Messages.
   var payload = event.postback.payload;
 
-  console.log("[receivedPostback] from user (%d) on page (%d) with payload ('%s') " + 
+  console.log("[receivedPostback] from user (%d) on page (%d) with payload ('%s') " +
     "at (%d)", senderID, recipientID, payload, timeOfPostback);
 
   respondToHelpRequestWithTemplates(senderID, payload);
@@ -459,8 +459,8 @@ function sendTextMessage(recipientId, messageText) {
 }
 
 /*
- * Call the Send API. The message data goes in the body. If successful, we'll 
- * get the message id in a response 
+ * Call the Send API. The message data goes in the body. If successful, we'll
+ * get the message id in a response
  *
  */
 function callSendAPI(messageData) {
@@ -476,16 +476,16 @@ function callSendAPI(messageData) {
       var messageId = body.message_id;
 
       if (messageId) {
-        console.log("[callSendAPI] Successfully sent message with id %s to recipient %s", 
+        console.log("[callSendAPI] Successfully sent message with id %s to recipient %s",
           messageId, recipientId);
       } else {
-      console.log("[callSendAPI] Successfully called Send API for recipient %s", 
+      console.log("[callSendAPI] Successfully called Send API for recipient %s",
         recipientId);
       }
     } else {
       console.error("[callSendAPI] Send API call failed", response.statusCode, response.statusMessage, body.error);
     }
-  });  
+  });
 }
 
 /*
@@ -523,12 +523,12 @@ function callSendProfile() {
     } else {
       console.error("[callSendProfile] Send profile call failed", response.statusCode, response.statusMessage, body.error);
     }
-  });  
+  });
 }
 
 /*
  * Start server
- * Webhooks must be available via SSL with a certificate signed by a valid 
+ * Webhooks must be available via SSL with a certificate signed by a valid
  * certificate authority.
  */
 app.listen(app.get('port'), function() {
@@ -537,4 +537,3 @@ app.listen(app.get('port'), function() {
 });
 
 module.exports = app;
-
