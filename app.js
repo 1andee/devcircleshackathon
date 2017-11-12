d4106fe881068fa2aeb077c331d4cf1b87144ffe
hkimca <harrison.kim.ca@gmail.com>
2017-11-11 20:19:54 -0500

    fix

--- app.js
+++ app.js
@@ -271,7 +271,7 @@ function showWhiteProducts (recipientId) {
   var products = shopify.product.list({ limit: 3});
   products.then(function(listOfProducts) {
         listOfProducts.forEach(function(product) {
-          console.log(product.id);
+          console.log("productLog: %s", product.id);
           var url = HOST_URL + "/product.html?id="+product.id;
           templateElements.push({
             title: product.title,
