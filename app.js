1996ac7ba05d0f092a1efe7187df0e4d5a810239
hkimca <harrison.kim.ca@gmail.com>
2017-11-11 19:24:48 -0500

    test

--- app.js
+++ app.js
@@ -201,7 +201,7 @@ function receivedMessage(event) {
         break;
 
       case 'white':
-        showWhiteProducts(senderID);
+        sendTextMessage(senderID, messsageText.toUpperCase());
         break;    
 
       default:
