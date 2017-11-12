87e834d66321efbd652be5c11b03825a3eee2d8e
Ian Duke <ian@thefarmventures.com>
2017-11-11 22:30:52 -0500

    comment out verify req sig

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const
 var app = express();
 app.set('port', process.env.PORT || 5000);
 app.set('view engine', 'ejs');
-app.use(bodyParser.json({ verify: verifyRequestSignature }));
+// app.use(bodyParser.json({ verify: verifyRequestSignature }));
 app.use(express.static('public'));
 
 /*
