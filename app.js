8c0b3dfb917f08928c6262dafdb852e4a3056fa5
Ian Duke <ian@thefarmventures.com>
2017-11-12 00:00:15 -0500

    log req.body.result

--- app.js
+++ app.js
@@ -129,7 +129,9 @@ app.post('/webhook', function (req, res) {
   // you're paused on a breakpoint! Otherwise, the request might time out.
   res.sendStatus(200);
 
-  console.log(JSON.stringify(req.body));
+  console.log(JSON.stringify('req.body.result', req.body.result));
+  console.log(JSON.stringify("req.body['result'].parameters", req.body['result'].parameters));
+  console.log(JSON.stringify("req.body['result']['parameters']", req.body['result']['parameters']));
   var data = req.body;
 
   // Make sure this is a page subscription
